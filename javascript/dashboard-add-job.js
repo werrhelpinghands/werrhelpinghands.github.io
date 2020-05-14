e964f69b123b552167a6d6160052ad4ed84110bc
Richard Job <richardjob05@gmail.com>
2020-05-14 21:03:47 +0530

    added feature to add job and get job

---
+++ javascript/dashboard-add-job.js
@@ -0,0 +1,77 @@
+async function addJob() {
+  let title = document.getElementById("title").value;
+  let category = document.getElementById("category").value;
+  let category = document.getElementById("category").options[category].text
+  let type = document.getElementById("type").value;
+  let type = document.getElementById("type").options[type].text
+  let tags = document.getElementById("tags").value;
+  let company = document.getElementById("company").value;
+  let website = document.getElementById("website").value;
+  let about = document.getElementById("about").value;
+  let roles = document.getElementById("roles").value;
+  let skills = document.getElementById("skills").value;
+  let location = document.getElementById("location").value;
+  let available = document.getElementById("available").value;
+  let duration = document.getElementById("duration").value;
+  let closingDate = document.getElementById("closingDate").value;
+  let pay = document.getElementById("pay").value;
+  let url = document.getElementById("url").value;
+  let portal = document.getElementById("portal").value;
+  let logo = document.getElementById("inputlogo").files[0];
+  let notification = document.getElementById("notification").files[0];
+
+  const formBody = new FormData();
+
+  formBody.set("title", title);
+  formBody.set("category", category);
+  formBody.set("type", type);
+  formBody.set("tags", tags);
+  formBody.set("company", company);
+  formBody.set("website", website);
+  formBody.set("about", about);
+  formBody.set("roles", roles);
+  formBody.set("skills", skills);
+  formBody.set("location", location);
+  formBody.set("available", available);
+  formBody.set("duration", duration);
+  formBody.set("expireAt", closingDate);
+  formBody.set("pay", pay);
+  formBody.set("url", url);
+  formBody.set("portal", portal);
+  formBody.append("file", logo);
+  formBody.append("file", notification);
+
+  await axios({
+    url: "http://localhost:3000/api/jobs/addJob",
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${
+        document.cookie
+          .split(";")
+          .filter((a) => a.includes("token"))[0]
+          .split("=")[1]
+      }`,
+    },
+    data: formBody,
+  }).then((res) => {
+    title = "";
+    category = "";
+    type = "";
+    tags = company = "";
+    website = "";
+    about = "";
+    roles = "";
+    skills = "";
+    location = "";
+    available = "";
+    duration = "";
+    closingDate = "";
+    pay = "";
+    url = "";
+    portal = "";
+    logo = "";
+    notification = "";
+    window.location.assign(`/job-page.html?id=${res.data._id}`)
+  });
+}
---
+++ javascript/job-page.js
@@ -0,0 +1,160 @@
+async function getJob() {
+  let _id = window.location.search.split("=")[1];
+  await axios({
+    url: `http://localhost:3000/api/jobs/${_id}`,
+    method: "GET",
+  }).then((res) => {
+    let data = res.data;
+    console.log(data);
+    let body = `<div id="titlebar">
+        <div class="container">
+            <div class="ten columns">
+                <h2>${data.title}&nbsp;<span class="full-time">${
+      data.type
+    }</span></h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+
+        <!-- Recent Jobs -->
+        <div class="eleven columns">
+            <div class="padding-right">
+
+                <!-- Company Info -->
+                <div class="company-info">
+                    <img src=${data.logo} alt="">
+                    <div class="content">
+                        <h4>${data.company}</h4>
+                        <span><a href=${
+                          data.website
+                        }><i class="fa fa-link"></i>Website</a></span>
+
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+
+                <p class="margin-reset">
+                <h3>About Company</h3>
+                ${data.about}
+                </p>
+
+                <br>
+                <h3>Internship Roles</h3>
+                <ul class="list-1">
+                    ${roles(data.roles)}
+                </ul>
+
+                <br>
+
+                <h4 class="margin-bottom-10">Job Requirment / Skills</h4>
+
+                <ul class="list-1">
+                    ${skills(data.skills)}
+                </ul>
+
+            </div>
+        </div>
+
+
+        <!-- Widgets -->
+        <div class="five columns">
+
+            <!-- Sort by -->
+            <div class="widget">
+                <h4>Overview</h4>
+
+                <div class="job-overview">
+
+                    <ul>
+                        <li>
+                            <i class="fa fa-map-marker"></i>
+                            <div>
+                                <strong>Location:</strong>
+                                <span>${data.location}</span>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fa fa-user"></i>
+                            <div>
+                                <strong>Job Title:</strong>
+                                <span>${data.title}</span>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="ln ln-icon-Add-User"></i>
+                            <div>
+                                <strong>
+                                    <Table>Available Applications:</Table>
+                                </strong>
+                                <span>${data.available}</span>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fa fa-clock-o"></i>
+                            <div>
+                                <strong>Duration</strong>
+                                <span>${data.duration}</span>
+                            </div>
+                        </li>
+                        <li>
+                        <li>
+                            <i class="ln ln-icon-Close-Window"></i>
+                            <div>
+                                <strong>Closing Date</strong>
+                                <span>${data.expireAt}</span>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fa fa-money"></i>
+                            <div>
+                                <strong>Pay</strong>
+                                <span>${data.pay}</span>
+                            </div>
+                        </li>
+                    </ul>
+
+
+                    <a href=${data.portal} class="button">Apply For This Job</a>
+                    <a href=${data.notification} class="button">Download Notification</a>
+
+                </div>
+
+            </div>
+            <div class="eight columns">
+                <ul class="social-icons">
+                    <li style="margin-left: 10%;"><a class="facebook" href="#"><i class="icon-facebook"></i></a>
+                    </li>
+                    <li style="margin-left: 10px;"><a class="twitter" href="#"><i class="icon-twitter"></i></a>
+                    </li>
+                    <li style="margin-left: 10px;"><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+                    <li style="margin-left: 10px;"><a class="linkedin" href="#"><i
+                                class="icon-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Widgets / End -->
+    </div>`;
+
+
+    document.getElementById("jobContent").innerHTML = body;
+  });
+}
+
+function roles(val) {
+  let string = "";
+  val.forEach((role) => {
+    string = `${string}<li>${role}</li>`;
+  });
+  return string;
+}
+
+function skills(val) {
+  let string = "";
+  val.forEach((skill) => {
+    string = `${string}<li>${skill}</li>`;
+  });
+  return string;
+}
