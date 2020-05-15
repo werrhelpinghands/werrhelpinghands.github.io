0c6db5772a73ccaba4e415c64008d3b7a09a1ea4
Richard Job <richardjob05@gmail.com>
2020-05-15 20:21:42 +0530

    added logout to index admin

--- javascript/index_job_admin.js
+++ javascript/index_job_admin.js
@@ -24,3 +24,9 @@ async function getJobs() {
     document.getElementById('jobs-container').innerHTML = body
   });
 }
+
+function logout() {
+  localStorage.removeItem('admin')
+  localStorage.removeItem('token')
+  window.location.replace('/login.html')
+}
