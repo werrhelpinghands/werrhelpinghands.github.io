e17d0ada233bcf19b23be465c5e488c32e841dad
Richard Job <richardjob05@gmail.com>
2020-05-15 20:48:17 +0530

    implimented jobs page admin

---
+++ javascript/browse-jobs_admin.js
@@ -0,0 +1,32 @@
+async function getJobs() {
+    await axios({
+      url: "https://helpinghands-server.herokuapp.com/api/jobs",
+      method: "GET",
+    }).then((res) => {
+      console.log(res.data);
+      let body = "";
+      res.data.forEach((post) => {
+        body = `${body} 
+              <a href="job-page.html?id=${post._id}" class="listing full-time">
+                              <div class="listing-logo">
+                                  <img src=${post.logo} alt="">
+                              </div>
+                              <div class="listing-title">
+                                  <h6 style="font-weight: bold;">${post.title}</h6>
+                                  <h6>${post.company}</h6>
+                                  <h6><span class="listing-type">APPLY</span></h6>
+                                  <h6>${post.location}</h6>
+                                  <h6>${post.pay}</h6>
+                                  <h6>TYPE : ${post.type}</h6>
+                              </div>
+                          </a>`;
+      });
+      document.getElementById('jobs-container').innerHTML = body
+    });
+  }
+  
+  function logout() {
+    localStorage.removeItem('admin')
+    localStorage.removeItem('token')
+    window.location.replace('/login.html')
+  }
