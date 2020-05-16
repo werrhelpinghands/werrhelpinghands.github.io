64524387d55254388c6ed928355014d655dd37ba
Richard Job <richardjob05@gmail.com>
2020-05-16 16:03:44 +0530

    started working on job news

---
+++ javascript/add-job-news.js
@@ -0,0 +1,30 @@
+async function addNews() {
+  document.getElementById("postBtn").innerHTML = `Loading...`;
+  document.getElementById("postBtn").disabled = true;
+
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+  let url = document.getElementById("url").value;
+
+  const formBody = new FormData();
+
+  formBody.set("title", title);
+  formBody.set("description", description);
+  formBody.set("url", url);
+
+  await axios({
+    url: "https://helpinghands-server.herokuapp.com/api/jobs/news",
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    data: formBody,
+  }).then((res) => {
+    title = "";
+    description = "";
+    url = "";
+    document.getElementById("postBtn").disabled = false;
+    window.location.assign(`/dashboard-manage-applications_admin.html`);
+  });
+}
