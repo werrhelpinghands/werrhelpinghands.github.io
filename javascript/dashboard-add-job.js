959845fffc8397f3905c628d1cfe1a16d2b81aeb
muneer <purimuneer@gmail.com>
2020-05-18 19:55:49 +0530

    validations

--- javascript/dashboard-add-job.js
+++ javascript/dashboard-add-job.js
@@ -1,3 +1,111 @@
+function checkval(){
+  let title = document.getElementById("title").value;//
+  let category = document.getElementById("category").value;//
+  let type = document.getElementById("type").value;//
+  type = document.getElementById("type").options[type].text
+  let tags = document.getElementById("tags").value;//
+  let company = document.getElementById("company").value;//
+  let website = document.getElementById("website").value;//
+  let location = document.getElementById("location").value;//
+  let closingDate = document.getElementById("closingDate").value;//
+  let logo = document.getElementById("inputlogo").files[0];//
+  let notification = document.getElementById("notification").files[0];//
+
+if (title.trim() != ""){
+
+if (category.trim() != ""){
+
+  if(type.trim() != ""){
+
+    if(tags.trim() != ""){
+
+      if(company.trim() != ""){
+
+        if(website.trim() != ""){
+
+          if(location.trim() != ""){
+
+            if(closingDate.trim() != ""){
+
+
+                
+              addJob()
+
+                
+
+
+              
+
+            }else{
+              
+  alert("closing date cannot empty");
+  document.getElementById("closingDate").style.border="2px solid red";
+              return false;
+            }
+
+          }else{
+            
+  alert("location cannot empty");
+  document.getElementById("location").style.border="2px solid red";
+          return false;  
+        }
+
+
+        }else{
+
+          alert("website cannot empty");
+          document.getElementById("website").style.border="2px solid red";
+          return false;
+
+        }
+
+
+      }else{
+    
+  alert("company cannot empty");
+  document.getElementById("company").style.border="2px solid red";
+        return false;
+
+
+      }
+
+    }else{
+      
+      alert("tags cannot empty");
+      document.getElementById("tags").style.border="2px solid red";
+      return false;
+    }
+
+
+  }else{
+    
+  alert("type cannot empty");
+  document.getElementById("type").style.border="2px solid red";
+    return false;
+  }
+
+}else{
+
+  alert("category cannot be empty");
+  document.getElementById("category").style.border="2px solid red";
+  return false;
+}
+
+}else{
+  
+  alert("title is empty");
+  document.getElementById("title").style.border="2px solid red";
+  return false;
+}
+
+
+}
+
+
+
+
+
+
 async function addJob() {
 
   document.getElementById('postBtn').innerHTML = `Loading...`
