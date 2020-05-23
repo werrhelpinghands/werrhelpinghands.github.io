6af7cfff35a3e00331dd5576e0cb09a50f73cb91
Richard Job <richardjob05@gmail.com>
2020-05-23 13:01:47 +0530

    fixed a email typo

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,7 +17,7 @@ const sendRecoveryEmail = (email, code) => {
   var mailOptions = {
     from: process.env.SENDER_EMAIL,
     to: email,
-    subject: `Helpinh Hands Account Recovery`,
+    subject: `Helping Hands Account Recovery`,
     html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
     <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
