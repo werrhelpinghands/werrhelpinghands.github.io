2508a96e2e72ed6bac24375c20fc8cd28cc57830
Richard Job <richardjob05@gmail.com>
2020-05-26 19:47:13 +0530

    created auto delete News after 30days

--- models/news.js
+++ models/news.js
@@ -6,17 +6,13 @@ const newsSchema = new Schema(
     title: String,
     description: String,
     url: String,
-    expireAt: {
-        type: Number,
-        default: 10
-    }
   },
   {
     timestamps: true,
   }
 );
 
-newsSchema.index({ expireAfterSeconds: this.expireAt });
+newsSchema.index({ expireAfterSeconds: 2592000 });
 
 var News = mongoose.model("News", newsSchema);
 module.exports = News;
