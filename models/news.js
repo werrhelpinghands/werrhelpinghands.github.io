cd97fb5520ddfb57674a82d5f932439ff3d4a63f
Richard Job <richardjob05@gmail.com>
2020-05-26 19:36:46 +0530

    test 7

--- models/news.js
+++ models/news.js
@@ -6,13 +6,17 @@ const newsSchema = new Schema(
     title: String,
     description: String,
     url: String,
+    date: {
+        type: Number,
+        default: 30
+    }
   },
   {
     timestamps: true,
   }
 );
 
-newsSchema.index({ expireAfterSeconds: 60 });
+newsSchema.index({ expireAfterSeconds: this.date });
 
 var News = mongoose.model("News", newsSchema);
 module.exports = News;
