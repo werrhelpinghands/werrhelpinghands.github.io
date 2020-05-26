d2b772cc7fc0b499d597c67dc99e5c6d4ab0f80c
Richard Job <richardjob05@gmail.com>
2020-05-26 20:01:42 +0530

    created auto delete of jobs

--- models/job.js
+++ models/job.js
@@ -1,81 +1,92 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const jobSchema = new Schema({
-    title:{
-        type: String,
-        required: true
+const jobSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    type:{
-        type: String,
-        required: true
+    type: {
+      type: String,
+      required: true,
     },
-    category:{
-        type: String,
-        required: true
+    category: {
+      type: String,
+      required: true,
     },
-    tags:{
-        type: Array,
-        default: []
+    tags: {
+      type: Array,
+      default: [],
     },
-    company:{
-        type: String,
-        required: true
+    company: {
+      type: String,
+      required: true,
     },
-    website:{
-        type: String,
+    website: {
+      type: String,
     },
-    about:{
-        type: String,
-        required: true
+    about: {
+      type: String,
+      required: true,
     },
-    roles:{
-        type: Array,
-        default: []
+    roles: {
+      type: Array,
+      default: [],
     },
-    skills:{
-        type: Array,
-        default: []
+    skills: {
+      type: Array,
+      default: [],
     },
-    location:{
-        type: String,
-        required: true
+    location: {
+      type: String,
+      required: true,
     },
-    duration:{
-        type: String,
-        required: true
+    duration: {
+      type: String,
+      required: true,
     },
-    pay:{
-        type: String,
-        required: true
+    pay: {
+      type: String,
+      required: true,
     },
-    available:{
-        type: String,
-        required: true
+    available: {
+      type: String,
+      required: true,
     },
-    expireAt:{
-        type: Date,
-        required: true
+    expireAt: {
+      type: Date,
+      required: true,
     },
-    url:{
-        type: String,
-        required: true
+    url: {
+      type: String,
+      required: true,
     },
-    logo:{
-        type: String,
-        required: true
+    logo: {
+      type: String,
+      required: true,
     },
-    notification:{
-        type: String,
-        required: true
+    notification: {
+      type: String,
+      required: true,
     },
-    portal:{
-        type: String,
-        required: true
-    }
-},{
-    timestamps: true
-})
+    portal: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+jobSchema.methods = {
+  setExpiry: function () {
+    return Math.abs(this.expireAt - this.createdAt) / 1000;
+  },
+};
+
+jobSchema.index({ expireAfterSeconds: this.setExpiry() });
 
 var Job = mongoose.model("Job", jobSchema);
 module.exports = Job;
