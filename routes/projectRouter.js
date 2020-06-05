2f91e5786c8f87e0c86c0a5c751e5efacfcca1dc
Richard Job <richardjob05@gmail.com>
2020-06-05 13:57:48 +0530

    fixed a error

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var projectRouter = express.Router();
 const { isSignedIn, isAdmin } = require("../controllers/auth");
-const { addProject, getProject, getAllProjects } = require("../controllers/project");
+const {
+  addProject,
+  getProject,
+  getAllProjects,
+} = require("../controllers/project");
 
 projectRouter.post("/addProject", isSignedIn, isAdmin, addProject);
 
-projectRouter.get('/:id', getProject)
+projectRouter.get("/:id", getProject);
 
-projectRouter.get('/allProjects', getAllProjects)
+projectRouter.get("/allProjects", getAllProjects);
