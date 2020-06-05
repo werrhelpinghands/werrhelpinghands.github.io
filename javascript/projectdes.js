212ab0765b95462851922a0cdafcb75eed61ebf4
Richard Job <richardjob05@gmail.com>
2020-06-05 15:41:44 +0530

    fixed project description page

--- javascript/projectdes.js
+++ javascript/projectdes.js
@@ -26,49 +26,81 @@ async function loadProject() {
       <div><span class="pub-d">Contact : </span><span class="pub-da">${data.contactEmail}</span></div>
     </div>
     <div class="social-lin">
-      <!-- AddToAny BEGIN -->
+ 
       <div class="linkssocial">
         <a href="#"><img class="linksocialicons" src="./images/facebook.png" alt="social share links "></a>
         <a href="#"><img class="linksocialicons" src="./images/linkedin.png" alt="social share links "></a>
         <a href="#"><img class="linksocialicons" src="./images/twitter.png" alt="social share links "></a>
       </div>
-      <!-- AddToAny END -->
+
       <div class="tsurvey">
         <a href="https://forms.gle/HZhbibgeLb5RgtJu6"><button class="bt-ppts">TAKE SURVEY</button></a>
       </div>
     </div>
     <div class="commentsecmain">
       <button id="btnn1" class="maincommentbtn btnn1"><i class="fas fa-comment"></i> &nbsp; COMMENTS</button>
-      <button class="maincommentbtn btnn2"><i class="fas fa-thumbs-up"></i></button>
-      <p style="margin-top: 13px;">40 upvotes</p>
+      <button class="maincommentbtn btnn2" onclick="addLike()"><i class="fas fa-thumbs-up"></i></button>
+      <p style="margin-top: 13px; id="likes">${data.likes} upvotes</p>
     </div>
     <div id="slidersec">
       <div class="commentssec">
         <div class="formclass">
-          <form action="">
+          <form id="newComment">
             <input id="formtextbox" placeholder="Share your thoughts...." type="text"> <input id="formtextbutton"
-              type="button" value="SUBMIT">
+              type="button" onclick="addComment()" value="SUBMIT">
           </form>
         </div>
       </div>
-      <div class="commentsfromform">
-        <div class="commentform">
-          <h4>User</h4>
-          <h5>this is a comment</h5>
-        </div>
-        <div class="commentform">
-          <h4>User</h4>
-          <h5>this is a comment</h5>
-        </div>
-        <div class="commentform">
-          <h4>User</h4>
-          <h5>this is a comment</h5>
-        </div>
-      </div>
+      <div class="commentsfromform">`;
+
+      data.comments.reverse();
+      data.comments.forEach((comment) => {
+        html =
+          html +
+          `<div class="commentform">
+      <h4>${comment.user}</h4>
+      <h5>${comment.comment}</h5>
     </div>`;
+      });
+      html =html + `</div></div>`;
       document.getElementById("projectConatiner").innerHTML = html;
+      document.getElementById("newComment").hidden = localStorage.getItem('token') ? true : false
     })
     .catch((err) => {
       console.log("error", err);
     });
+}
+
+function addComment(e) {
+  let _id = window.location.search.split("=")[1];
+  let body = {
+    comment: document.getElementById('formtextbox').value
+  }
+  await axios({
+    url: `https://helpinghands-server.herokuapp.com/api/projects/addComment/${_id}`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    data: body,
+  }).then(()=>{
+    window.location.assign(window.location.reload())
+  })
+}
+
+function addLike() {
+  let _id = window.location.search.split("=")[1];
+  await axios({
+    url: `https://helpinghands-server.herokuapp.com/api/projects/addLike/${_id}`,
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    data: body,
+  }).then(()=>{
+    let upvote = document.getElementById('formtextbox').value
+    let newCount = parseInt(upvote.split(" ")[0]) + 1
+    document.getElementById('formtextbox').value = `${newCount} upvotes`
+  })
 }
